157406c4197410185f0b42f1fdd14d212c2dd0c4
john <misonjohnemmanuel@gmail.com>
2025-12-11 00:21:41 +0800

    employee search function

--- app/components/hris/EmployeeList.jsx
+++ app/components/hris/EmployeeList.jsx
@@ -14,6 +14,8 @@ export default function EmployeeList(){
     dayjs.extend(relativeTime);
     const [ addNewEmployeeModal, showAddNewEmployeeModal ] = useState(false);
     const [ employeeList, setEmployeeList ] = useState([]);
+    const [search, setSearch] = useState("");
+    const [isEmployeeListLoading, setIsEmployeeListLoading ] = useState(true);
 
     useEffect(()=>{
         if(addNewEmployeeModal){
@@ -23,10 +25,21 @@ export default function EmployeeList(){
         }
     }, [addNewEmployeeModal])
 
-    const getEmployeeList = async ()=>{
+    const getEmployeeList = async (searchkeyword=null, per_page=10, page=1)=>{
         try{
-            const res = await api.get("/employees");
-            setEmployeeList(res.data);
+            const params = {
+                page,
+                per_page
+            };
+
+            if(searchkeyword){
+                params.search = searchkeyword;
+            }
+
+            const queryString = new URLSearchParams(params).toString();
+            const res = await api.get("/employees?"+queryString);
+            setEmployeeList(res.data.data);
+            setIsEmployeeListLoading(false)
         }catch(err){
             console.log(err)
         }
@@ -36,6 +49,17 @@ export default function EmployeeList(){
         getEmployeeList();
     }, [])
 
+    useEffect(() => {
+        setIsEmployeeListLoading(true)
+        const timeout = setTimeout(() => {
+            getEmployeeList(search);
+        }, 500);
+
+        return () => clearTimeout(timeout);
+    }, [search]);
+
+    
+
     return (
         <>
             <div className="w-100 d-flex justify-content-center">
@@ -43,56 +67,70 @@ export default function EmployeeList(){
                     <div className="col-lg-9 mx-auto">
                         <div className="d-flex justify-content-end mt-5">
                             <div>
-                                <input type="search" class="employee-list-table-search" placeholder="Search ..." />
+                                <input type="search" class="employee-list-table-search" onChange={ (e) => setSearch(e.target.value)} placeholder="Search ..." />
                             </div>
                             <button className="ms-2 btn1" onClick={()=>showAddNewEmployeeModal(true)}><UserPlus size={20} /> Add new employee</button>
                         </div>
                         <div className="rounded overflow-hidden mt-2">
+                            
+                           
                             {
-                                employeeList.length ? 
-                                    <table className="employee-list-table ">
-                                        <thead>
+                            <table className="employee-list-table ">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Name</th>
+                                        <th>Position</th>
+                                        <th>Status</th>
+                                        <th>Date Added</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        isEmployeeListLoading ? 
                                             <tr>
-                                                <th></th>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Status</th>
-                                                <th>Date Added</th>
-                                                <th>Action</th>
+                                                <td colSpan={6}><LoadingContainer /></td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                employeeList?.map((value, index)=>{
-                                                    return (
-                                                        <tr>
-                                                            <td>
-                                                                <div className="employee-list-image d-flex justify-content-center align-items-center">
-                                                                    <UserRound />
-                                                                </div>
-                                                            </td>
-                                                            <td><span className="text-uppercase fw-bold">{value.lastname}, {value.firstname} {value.middlename}</span></td>
-                                                            <td><span className="text-info">Web Developer</span></td>
-                                                            <td><span className="text-success fw-bold">Active</span></td>
-                                                            <td>{dayjs(value.created_at).format("MMM, DD. YYYY")}</td>
-                                                            <td>
-                                                                <div className="d-flex">
-                                                                    <button className="employee-list-table-button me-2">
-                                                                        <EyeIcon size={15} />
-                                                                    </button>
-                                                                    <button className="employee-list-table-button">
-                                                                        <SquarePen size={15} />
-                                                                    </button>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                })
-                                            }
-                                        </tbody>
-                                    </table>
-                                :
-                                    <LoadingContainer />
+                                        : 
+                                            employeeList.length > 0 ? 
+                                            employeeList.map((value, index)=>{
+                                            return (
+                                                <tr>
+                                                    <td>
+                                                        <div className="employee-list-image d-flex justify-content-center align-items-center">
+                                                            <UserRound />
+                                                        </div>
+                                                    </td>
+                                                    <td><span className="text-uppercase fw-bold">{value.lastname}, {value.firstname} {value.middlename}</span></td>
+                                                    <td><span className="text-info">Web Developer</span></td>
+                                                    <td><span className="text-success fw-bold">Active</span></td>
+                                                    <td>{dayjs(value.created_at).format("MMM, DD. YYYY")}</td>
+                                                    <td>
+                                                        <div className="d-flex">
+                                                            <button className="employee-list-table-button me-2">
+                                                                <EyeIcon size={15} />
+                                                            </button>
+                                                            <button className="employee-list-table-button">
+                                                                <SquarePen size={15} />
+                                                            </button>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+
+                                        :
+
+                                        <tr>
+                                            <td colSpan={6}><p className="text-center m-0">No data.</p></td>
+                                        </tr>
+                                    }       
+
+                                    
+                                </tbody>
+                            </table>
+                                
                             }
 
                             
