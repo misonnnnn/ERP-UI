c020b41adf94d7aba0b789d7e7189285fae72748
john <misonjohnemmanuel@gmail.com>
2025-12-13 02:01:23 +0800

    update documents UI

---
+++ app/components/dms/CreateNewFolder.jsx
@@ -0,0 +1,84 @@
+import { FolderPlus, Save, UserPlus } from "lucide-react"
+import { useState } from "react";
+import api from "@/lib/api";
+import { toast } from "sonner";
+
+export default function CreateNewFolder({ closeModal, reinitializeFolderList }){
+    const [name, setName] = useState(null);
+    
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        // basic validation
+        if (!name) {
+            toast.error("Please fill in all required fields.")
+            return;
+        }
+
+        setLoading(true);
+
+        try {
+        const formData = new FormData();
+            formData.append("name", name);
+
+            const res = await api.post("/folder", formData);
+
+            if(res.data){
+                if(res.data.id){
+                    toast.success("Folder created successfully")
+                    closeModal()
+                    setName("");
+                    reinitializeFolderList();
+                } 
+            
+            }else{
+                toast.error("Failed to create folder.")
+            }
+
+        } catch (err) {
+            const message = err?.response?.data?.message;
+            if (message) {
+                toast.error(message);
+            } else {
+                toast.error("Failed to create folder.");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h6><FolderPlus /> Create new Folder</h6>
+                    <hr />
+                    <div className="row m-0 mt-3">
+                        <div className="col-lg-12 mt-3" >
+                            <div>
+                                <label htmlFor="">Folder name <span className="text-danger">*</span></label>
+                                <input 
+                                    type="text" 
+                                    className="form-control form-control-sm  shadow-none" 
+                                    placeholder="Folder name" 
+                                    onChange={(e)=>setName(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-end">
+                        <div className="d-flex">
+                            <div className="btn btn-sm btn-secondary me-2" onClick={()=> closeModal()}>Cancel</div>
+                            <button className="btn btn-sm btn-primary"><Save size={15} strokeWidth={2} /> {loading ? "Saving..." : "Save"}</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+}
--- app/components/dms/Documents.jsx
+++ app/components/dms/Documents.jsx
@@ -1,97 +1,91 @@
 "use client"
+import { ChevronRight, FilePlusCorner, FolderPlus, Slash } from "lucide-react"
 import "../../css/dms/documents.css"
+import "../../css/modal.css"
+import CreateNewFolder from "./CreateNewFolder"
+import { useEffect, useState } from "react"
+import api from "@/lib/api"
 
 export default function Documents(){
+    const [ isCreateNewFolderModalActive, setIsCreateNewFolderModalActive ] = useState(false);
+    const [ folderList, setFolderList ] = useState([]);
+    
+    const getFolderList = async () =>{
+        const res = await api.get("/folder?");
+        if(res){
+            console.log(res)
+            setFolderList(res.data);
+        }
+    }
+
+    useEffect(() =>{
+        getFolderList();
+    }, [])
+
     return (
-        <div class="container" style={{marginTop:'80px'}}>
-            <div className="position-relative">
-                <div className="row">
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
+        <>
+            <div className="container" >
+                <div className="d-flex justify-content-start mt-2">
+                    <button className="documents_button me-2">
+                        Upload File 
+                        <FilePlusCorner size={17} className="ms-1"/>
+                    </button>
 
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
+                    <button className="documents_button me-2" onClick={() => setIsCreateNewFolderModalActive(true)}>
+                        Create New Folder
+                        <FolderPlus size={17}  className="ms-1"/>
+                    </button>
+                </div>
+                <hr />
+                
+                {/* breadcrumbs */}
+                
+                <div>
+                    <p className="m-0"><span className="path-text me-3">Path:</span> Home <ChevronRight size={10} /> test <ChevronRight size={10} /></p>
+                </div>
 
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
+                <div className="position-relative mt-5" >
+                    <div className="row ">
+                        {
+                            folderList?.map((folder, index) =>{
+                                return (
+                                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
+                                        <div className="folder">
+                                            <div className="folder-inside"></div>
+                                            <span className="folder-name">{folder.name}</span>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                        
+                        
                     </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-2 col-md-4 col-sm-6 folder-outer">
-                        <div class="folder">
-                            <div class="folder-inside"></div>
-                            <span className="folder-name">Folder1</span>
-                        </div>
-                    </div>
-                    
                 </div>
             </div>
-        </div>
+
+            {
+                isCreateNewFolderModalActive ? 
+                <>
+                    <div className="modal-background" onClick={ () => setIsCreateNewFolderModalActive(false)}></div>
+                    <div className="modal-div">
+                        <div className="row m-0 w-100">
+                            <div className="col-lg-3 mx-auto ">
+                                <div className="modal-div-content">
+                                    <div className="modal-div-close" onClick={ () => setIsCreateNewFolderModalActive(false)}> X </div>
+                                    {/* <AddNewEmployee /> */}
+                                    <CreateNewFolder  
+                                        closeModal={() => setIsCreateNewFolderModalActive(false)}
+                                        reinitializeFolderList = { () => getFolderList()} 
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+
+                : ''
+            }
+        </>
     )
 }
